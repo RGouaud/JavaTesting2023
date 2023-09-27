8af55d26ef886bac1546e448dd8288c7c1021d5e
rgouaud <r.gouaud@gmail.com>
2023-09-27 19:17:47 +0200

    creation de la classe calculator

---
+++ src/main/java/calculator/Calcultator.java
@@ -0,0 +1,12 @@
+package calculator;
+
+public class Calcultator {
+
+    public int add(int opG, int opD){
+        return opG+opD;
+    }
+
+    public int divide(int opG, int opD){
+        return opG/opD;
+    }
+}
