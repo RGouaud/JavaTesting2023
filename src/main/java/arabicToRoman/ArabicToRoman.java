175addfa5c1ef19ec6242fb334b004c54ed552a1
rgouaud <r.gouaud@gmail.com>
2023-10-10 17:36:30 +0200

    validation des tests pour 4 et 9

--- src/main/java/arabicToRoman/ArabicToRoman.java
+++ src/main/java/arabicToRoman/ArabicToRoman.java
@@ -9,9 +9,17 @@ public class ArabicToRoman {
             arabicNumber -= 10;
             romanNumber += "X";
         }
+        if(arabicNumber==9){
+            arabicNumber -= 9;
+            romanNumber += "IX";
+        }
         if(arabicNumber==5){
             arabicNumber -= 5;
             romanNumber += "V";
+        }
+        if(arabicNumber==4){
+            arabicNumber -= 4;
+            romanNumber += "IV";
         }
         while(arabicNumber <=3 && arabicNumber >0){
             arabicNumber -= 1;
