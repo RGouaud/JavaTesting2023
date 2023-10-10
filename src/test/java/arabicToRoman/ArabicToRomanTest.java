df4826f4dae30fa269a9cdda76304f1269e3e627
rgouaud <r.gouaud@gmail.com>
2023-10-10 17:18:22 +0200

    écriture des tests pour 10,20,30

--- src/test/java/arabicToRoman/ArabicToRomanTest.java
+++ src/test/java/arabicToRoman/ArabicToRomanTest.java
@@ -44,4 +44,33 @@ class ArabicToRomanTest {
         //Then
         assertThat(romanNumber).isEqualTo("III");
     }
+
+    @Test
+    void conversion_10(){
+        //Given
+        ArabicToRoman convertisseur = new ArabicToRoman();
+        //When
+        String romanNumber = convertisseur.convert(10);
+        //Then
+        assertThat(romanNumber).isEqualTo("X");
+    }
+
+    @Test
+    void conversion_20() {
+        //Given
+        ArabicToRoman convertisseur = new ArabicToRoman();
+        //When
+        String romanNumber = convertisseur.convert(20);
+        //Then
+        assertThat(romanNumber).isEqualTo("XX");
+    }
+    @Test
+    void conversion_30() {
+        //Given
+        ArabicToRoman convertisseur = new ArabicToRoman();
+        //When
+        String romanNumber = convertisseur.convert(30);
+        //Then
+        assertThat(romanNumber).isEqualTo("XXX");
+    }
 }
