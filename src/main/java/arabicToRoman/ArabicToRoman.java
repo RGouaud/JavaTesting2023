d25560067f56a0ee273915aa88cdf4f6a7927ca6
rgouaud <r.gouaud@gmail.com>
2023-10-10 17:50:09 +0200

    refactorisation du code, passage des tests

--- src/main/java/arabicToRoman/ArabicToRoman.java
+++ src/main/java/arabicToRoman/ArabicToRoman.java
@@ -1,29 +1,23 @@
 package arabicToRoman;
 
+import java.util.List;
+
 public class ArabicToRoman {
     public String convert(int arabicNumber){
 
         String romanNumber = "";
+        String[] roman = {"I","IV","V","IX","X"};
+        int[] arabic = {1,4,5,9,10};
+        int i = roman.length-1;
 
-        while(arabicNumber <=30 && arabicNumber>=10){
-            arabicNumber -= 10;
-            romanNumber += "X";
-        }
-        if(arabicNumber==9){
-            arabicNumber -= 9;
-            romanNumber += "IX";
-        }
-        if(arabicNumber==5){
-            arabicNumber -= 5;
-            romanNumber += "V";
-        }
-        if(arabicNumber==4){
-            arabicNumber -= 4;
-            romanNumber += "IV";
-        }
-        while(arabicNumber <=3 && arabicNumber >0){
-            arabicNumber -= 1;
-            romanNumber += "I";
+        while(arabicNumber>0 && arabicNumber <= 38){
+            if(arabicNumber >= arabic[i]){
+                romanNumber += roman[i];
+                arabicNumber -= arabic[i];
+            }
+            else{
+                i--;
+            }
         }
         if(romanNumber.isEmpty()){
             return null;
